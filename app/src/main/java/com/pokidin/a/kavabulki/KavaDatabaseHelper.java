bbb57003043acc9bd4076f580e4da7d6c8f9cd60
AlexanderPokidin <a.pokidin@gmail.com>
2018-03-31 21:40:19 +0300

    Database updated

--- app/src/main/java/com/pokidin/a/kavabulki/KavaDatabaseHelper.java
+++ app/src/main/java/com/pokidin/a/kavabulki/KavaDatabaseHelper.java
@@ -22,9 +22,38 @@ public class KavaDatabaseHelper extends SQLiteOpenHelper {
                 + "DESCRIPTION_RESOURCE_ID INTEGER, "
                 + "FAVORITE NUMERIC);");
 
+        insertDrink(db, "Espresso 30 ml", 12, R.drawable.espresso_item, R.string.espresso);
+
+        insertDrink(db, "Americano 175 ml", 12, R.drawable.americano_item, R.string.americano);
+        insertDrink(db, "Americano 270 ml", 20, R.drawable.americano_item, R.string.americano);
+        insertDrink(db, "Americano 400 ml", 22, R.drawable.americano_item, R.string.americano);
+
+        insertDrink(db, "Raf 175 ml", 25, R.drawable.raf_item, R.string.raf);
+        insertDrink(db, "Raf 270 ml", 30, R.drawable.raf_item, R.string.raf);
+        insertDrink(db, "Raf 400 ml", 40, R.drawable.raf_item, R.string.raf);
+
+        insertDrink(db, "Latte 270 ml", 20, R.drawable.latte_item, R.string.latte);
+        insertDrink(db, "Latte 400 ml", 22, R.drawable.latte_item, R.string.latte);
+        insertDrink(db, "Latte 500 ml", 30, R.drawable.latte_item, R.string.latte);
+
+        insertDrink(db, "Lavender latte 270 ml", 25, R.drawable.lavlate_item, R.string.lav_latte);
+        insertDrink(db, "Lavender latte 400 ml", 30, R.drawable.lavlate_item, R.string.lav_latte);
+
+        insertDrink(db, "Cappuccino 270 ml", 18, R.drawable.capuccino_item, R.string.capuccino);
+        insertDrink(db, "Cappuccino 400 ml", 22, R.drawable.capuccino_item, R.string.capuccino);
+        insertDrink(db, "Cappuccino 500 ml", 30, R.drawable.capuccino_item, R.string.capuccino);
+
+        insertDrink(db, "Flat white 270 ml", 25, R.drawable.flatewhite_item, R.string.flat_white);
+
         insertDrink(db, "Cacao 270 ml", 20, R.drawable.cacao_item, R.string.cacao);
         insertDrink(db, "Cacao 400 ml", 22, R.drawable.cacao_item, R.string.cacao);
         insertDrink(db, "Cacao 500 ml", 30, R.drawable.cacao_item, R.string.cacao);
+
+        insertDrink(db, "Hot chocolate 270 ml", 25, R.drawable.chocolate_item, R.string.chocolate);
+        insertDrink(db, "Hot chocolate 400 ml", 35, R.drawable.chocolate_item, R.string.chocolate);
+
+        insertDrink(db, "Coffee juice 270 ml", 25, R.drawable.kavajuice_item, R.string.juice);
+        insertDrink(db, "Coffee juice 400 ml", 30, R.drawable.kavajuice_item, R.string.juice);
     }
 
     @Override
